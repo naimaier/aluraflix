3d24cbb67ff41befb1d31cc2dcb5ef48a8eb2489
Henrique Naimaier <coubron@yahoo.com.br>
2022-10-24 10:25:02 -0300

    First commit

---
+++ src/test/java/naimaier/aluraflix/AluraflixApplicationTests.java
@@ -0,0 +1,13 @@
+package naimaier.aluraflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
