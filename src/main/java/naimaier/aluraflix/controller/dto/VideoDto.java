9c8b6f7207cd84585d4f2331304fad8431425fb4
Henrique Naimaier <coubron@yahoo.com.br>
2022-11-01 11:45:14 -0300

    Added category to video dto

--- src/main/java/naimaier/aluraflix/controller/dto/VideoDto.java
+++ src/main/java/naimaier/aluraflix/controller/dto/VideoDto.java
@@ -2,6 +2,7 @@ package naimaier.aluraflix.controller.dto;
 
 import org.springframework.data.domain.Page;
 
+import naimaier.aluraflix.model.Categoria;
 import naimaier.aluraflix.model.Video;
 
 public class VideoDto {
@@ -10,12 +11,14 @@ public class VideoDto {
 	private String titulo;
 	private String descricao;
 	private String url;
+	private Categoria categoria;
 	
 	public VideoDto(Video video) {
 		this.id = video.getId();
 		this.titulo = video.getTitulo();
 		this.descricao = video.getDescricao();
 		this.url = video.getUrl();
+		this.categoria = video.getCategoria();
 	}
 
 	public Long getId() {
@@ -32,6 +35,10 @@ public class VideoDto {
 
 	public String getUrl() {
 		return url;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
 	}
 
 	public static Page<VideoDto> convert(Page<Video> videos) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
-INSERT INTO videos (titulo, descricao, url) VALUES ('Video de teste', 'Primeiro teste', 'http://www.dw.com');
-INSERT INTO videos (titulo, descricao, url) VALUES ('Teste 2', 'Segundo teste', 'http://www.pudim.com.br');
+INSERT INTO categorias(nome) VALUES ('categoria 1');
+INSERT INTO categorias(nome) VALUES ('categoria 2');
+
+INSERT INTO videos (titulo, descricao, url, categoria_id) VALUES ('Video de teste', 'Primeiro teste', 'http://www.dw.com', 1);
+INSERT INTO videos (titulo, descricao, url, categoria_id) VALUES ('Teste 2', 'Segundo teste', 'http://www.pudim.com.br', 2);
+INSERT INTO videos (titulo, descricao, url, categoria_id) VALUES ('Teste 3', 'Terceiro teste', 'http://www.alura.com.br', 1);
