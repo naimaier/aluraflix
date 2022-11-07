8368e37643a1f557e9d008a640cc85efe1f9b9ae
Henrique Naimaier <coubron@yahoo.com.br>
2022-11-07 09:45:15 -0300

    Modified sql to add new fields

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO categorias(nome) VALUES ('categoria 1');
-INSERT INTO categorias(nome) VALUES ('categoria 2');
+INSERT INTO categorias(titulo, cor) VALUES ('categoria 1', '000');
+INSERT INTO categorias(titulo, cor) VALUES ('categoria 2', 'fff');
 
 INSERT INTO videos (titulo, descricao, url, categoria_id) VALUES ('Video de teste', 'Primeiro teste', 'http://www.dw.com', 1);
 INSERT INTO videos (titulo, descricao, url, categoria_id) VALUES ('Teste 2', 'Segundo teste', 'http://www.pudim.com.br', 2);
